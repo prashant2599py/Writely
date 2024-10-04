826a2b97a9cfcbb2129207934b5b5e6c979b2a86
Prashant Lodhi <prashantrajput2517@gmail.com>
2024-10-04 15:50:57 +0530

    untrack config

--- frontend/src/config.ts
+++
@@ -1,2 +0,0 @@
-// export const BACKEND_URL = "https://avtot5e2k4.execute-api.ap-southeast-2.amazonaws.com/default/Blogs-Fetch";
-// export const BACKEND_URL = "https://backend.prashantlodhi2599.workers.dev";
